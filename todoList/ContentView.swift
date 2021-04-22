1d8b60a179af65ff8b6693bb47868721bade14ff
Bilal BAig <bilalbaig587@gmail.com>
2021-04-22 10:55:45 -0500

    Initial Commit

---
+++ todoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todoList
+//
+//  Created by Bilal Baig on 4/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todoList/todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListApp.swift
+//  todoList
+//
+//  Created by Bilal Baig on 4/22/21.
+//
+
+import SwiftUI
+
+@main
+struct todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
