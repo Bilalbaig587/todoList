496ab99cb205f1c62a08607b06dea50e4318bf30
Bilal BAig <bilalbaig587@gmail.com>
2021-04-27 10:56:35 -0500

    final product

--- todoList/ContentView.swift
+++ todoList/ContentView.swift
@@ -1,21 +1,66 @@
 //
 //  ContentView.swift
-//  todoList
+//  Assignment Notebook
 //
 //  Created by Bilal Baig on 4/22/21.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var assignmentList = AssignmentTask()
+    @State private var showingAddView = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(assignmentList.items) {item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.course)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                }
+                .onMove(perform: { indices, newOffset in
+                    assignmentList.items.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    assignmentList.items.remove(atOffsets: indexSet)
+                })
+            }
+            .sheet(isPresented: $showingAddView, content: {
+                AddAssignmentView(assignmentList: assignmentList)
+            })
+            .navigationBarTitle("Assignment Notebook")
+            .navigationBarItems(leading: EditButton(),
+                                trailing: Button(action: {
+                                                    showingAddView = true }) {
+                                    Image(systemName: "plus")
+                                })
+        }
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
+    
+
+
+}
+
+struct Assignment: Identifiable, Codable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
